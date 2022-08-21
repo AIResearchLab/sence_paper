abf4f8df2843c3727871cb07a0db79b2ffc806c5
robodave94 <David.Hinwood@canberra.edu.au>
2022-08-21 11:02:54 +0000

    updates

--- sence_com/pysrc/standup.py
+++ sence_com/pysrc/standup.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 import rospy
 import roboticstoolbox
-from sence_msgs import Target_Buffer
+from sence_msgs.msg import Target_Buffer, Target
+import time
 
 if __name__ == "__main__" :
-    print('hello')
+    time.sleep(15)
+    rospy.init_node('test_stand')
+    pub = rospy.Publisher('sence_target', Target_Buffer, queue_size=3)
+    i = Target_Buffer()
+    i.count = 1
+    j1 = Target()
+    j1.TARGET_ID = 1
+    j1.TARGET_POSITION = 0.71
+    j1.TARGET_VELOCITY = 0.3
+
+    pub.publish(i)
+
+    exit()
