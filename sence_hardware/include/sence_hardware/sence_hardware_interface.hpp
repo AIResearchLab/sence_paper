fd0de914e2579142d543ee571e6ea9c1e4f02b5e
Ben Worth <benjamin.worth@gmail.com>
2023-07-04 00:19:52 +1000

    made a bunch of changes tonight beginning to combine the track_robot code with dynamixel sdk stuff

--- sence_hardware/include/sence_hardware/sence_hardware_interface.hpp
+++ sence_hardware/include/sence_hardware/sence_hardware_interface.hpp
@@ -1,18 +1,34 @@
 #pragma once
 
+#define FREQUENCY_HERTZ 10
+
 namespace sence_hardware
 {
-class SenceHardwareInterface
+class SenceHardwareInterface :
+  public hardware_interface::RobotHW,
+  public hardware_interface::HardwareInterface
 {
 public:
-  SenceHardwareInterface();
-  ~SenceHardwareInterface();
+  SenceHardwareInterface(){}
+  ~SenceHardwareInterface(){}
 
-  void init();
-  void update();
-  void shutdown();
+  bool init(ros::NodeHandle& root_nh, ros::NodeHandle& robot_nh);
+  void read(const ros::Time& time, const ros::Duration& period);
+  void write(const ros::Time& time, const ros::Duration& period);
 
-private:
+  double get_update_duration(){
+    return (1 / FREQUENCY_HERTZ);
+  }
+
+protected:
+  hardware_interface::JointStateInterface joint_state_interface;
+  hardware_interface::VelocityJointInterface velocity_joint_interface;
+
+  // hardware joint handles
+  std::vector<HardwareJoint> joints;
+
+  // simulation flag
+  bool simulated;
 };
 
 }  // namespace sence_hardware
